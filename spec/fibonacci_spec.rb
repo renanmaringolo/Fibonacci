3e7b05565b0ec9e00192143356bb39e592949b2d
Henrique Morato <henrique.morato@campuscode.com.br>
2018-09-26 17:43:54 -0300

    Atualização do setup

--- spec/fibonacci_spec.rb
+++ spec/fibonacci_spec.rb
@@ -3,31 +3,31 @@ require 'spec_helper'
 describe Fibonacci do
   let(:fibonacci) { Fibonacci.new }
 
-  it 'retorna o elemento F(0) da sequencia de Fibonacci' do
+  it 'retorna o elemento F(0) da sequência de Fibonacci' do
     expect(fibonacci.element(0)).to eq 0
   end
 
-  it 'retorna o elemento F(5) da sequencia de Fibonacci' do
+  it 'retorna o elemento F(5) da sequência de Fibonacci' do
     expect(fibonacci.element(5)).to eq 5
   end
 
-  it 'retorna o elemento F(10) da sequencia de Fibonacci' do
+  it 'retorna o elemento F(10) da sequência de Fibonacci' do
     expect(fibonacci.element(10)).to eq 55
   end
 
-  it 'retorna o elemento F(15) da sequencia de Fibonacci' do
+  it 'retorna o elemento F(15) da sequência de Fibonacci' do
     expect(fibonacci.element(15)).to eq 610
   end
 
-  it 'returna os elementos de fibonnacci até o primeiro' do
+  it 'retorna os elementos de fibonnacci até o primeiro' do
     expect(fibonacci.elements(0)).to eq [0]
   end
 
-  it 'returna os elementos de fibonnacci até o quinto' do
+  it 'retorna os elementos de fibonnacci até o quinto' do
     expect(fibonacci.elements(5)).to eq [0, 1, 1, 2, 3, 5]
   end
 
-  it 'returna os elementos de fibonnacci até o decimo' do
+  it 'retorna os elementos de fibonnacci até o décimo' do
     expect(fibonacci.elements(10)).to eq [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
   end
 end

bfc1f6bedec721f1b96caae8d29e3862022eaab9
Renan Maríngolo <renan.maringolo@gmail.com>
2019-07-07 02:58:50 -0300

    fibFinish

--- lib/fibonacci.rb
+++ lib/fibonacci.rb
@@ -3,4 +3,12 @@ class Fibonacci
         return  n  if n <= 1 
         element( n - 1 ) + element( n - 2 )
     end 
+
+    def elements(n)
+        return [0] if n == 0
+        return [0] if n == 1
+        return [0, 1, 1] if n == 2
+        a = elements(n - 1) 
+        a << a[-2] + a[-1]
+    end
 end
